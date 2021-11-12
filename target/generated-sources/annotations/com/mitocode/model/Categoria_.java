3be07f165a51edc17519b8a50a0e62a5bc9345b4
danielmartinmingo <daniel.martin.mingo@alumnos.upm.es>
2021-11-12 20:37:53 +0100

    first commit

---
+++ target/generated-sources/annotations/com/mitocode/model/Categoria_.java
@@ -0,0 +1,15 @@
+package com.mitocode.model;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-11-12T20:35:34")
+@StaticMetamodel(Categoria.class)
+public class Categoria_ { 
+
+    public static volatile SingularAttribute<Categoria, Integer> codigo;
+    public static volatile SingularAttribute<Categoria, Boolean> estado;
+    public static volatile SingularAttribute<Categoria, String> nombre;
+
+}
